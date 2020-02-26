8286457fc55d7faf0508f5a6bd7d894e257e0875
hunterfxfox <hunterfxfox@gmail.com>
2020-02-26 05:41:03 -0300

    upando arquivos atualizados

--- _js/javascript.js
+++ _js/javascript.js
@@ -1,1 +1,24 @@
-java script principal
+class main (){
+	var vida, vidaMax, vidaCalc ;
+	
+	function start(){
+	vidaMax = 100 
+	vida = vidaMax;
+
+
+
+
+	}
+
+	function update(){
+		vida = (vida * 100 / vidaMax );
+		console.log(vida)
+
+
+	
+	}
+
+
+}
+a<- main();
+a<- new update();
