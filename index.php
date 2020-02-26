0b7bac08883122fd35ebc6d25819952b0cc8e4d5
hunterfxfox <hunterfxfox@gmail.com>
2020-02-26 05:38:15 -0300

    atualizando o menu da página principal

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<?php include('script.php'); ?>
+	<link rel="stylesheet" href="_css/reset.css">
+	<link rel="stylesheet" href="_css/style.css">
+	<script language="_js/javascript.js"></script>
+</head>
+<body>
+	<nav>
+		<label for="equiv" > &equiv; Menu</label>
+		<input type="checkbox" id="equiv" >
+		<div id="menu">
+			<details>
+				<summary> Clientes </summary>
+				<a href="#" target="palco">Cadastrar Clientes</a>
+				<a href="#" target="palco">Visularizar Clientes</a>
+
+			</details>
+			<details>
+				<summary>
+					Ordem de serviços
+				</summary>
+				<a href="#" target="palco"> Cadastrar O.S </a>
+				<a href="#" target="palco"> Visualizar O.S</a>
+			</details>
+		</div>
+
+
+
+	</nav>
+
+	<canvas>
+		<iframe width="100%" height="100%" src="" frameborder="0"></iframe>
+	</canvas>
+	
+</body>
+</html>
